5b19718dc6e35533314fca150a43f56aa2888e6d
PhilliesWorkLaptop <philipp.matten@gmx.de>
2023-02-17 17:20:32 +0100

    Early stage main.py re-config
    refactored all 2d reg functions to class methods and tested in main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # global imports 
 import os
+import glob
 import json
-# import matplotlib.pyplot as plt # debug
+import matplotlib.pyplot as plt # debug
 import argparse
 
+# custom imports
+from src.volumeregistration2dbased import VolumeRegistration2Dbased as vol2d
 
 
 def main():
     ### CONFIG AND ARG-PARSING ###
     parser = argparse.ArgumentParser(description='OCT Whole Volume Registration (OVolReg).')
     parser.add_argument("-n", "--name", type=str, help="Experiment name.", default='test')
-    parser.add_argument("-c", "--config", default='./config.json', help="Path to config file with default experiment parameters.")
+    parser.add_argument("-c", "--config", default='config.json', help="Path to config file with default experiment parameters.")
     
     # get the arguments
     args = parser.parse_args() # access via args.argument
@@ -19,24 +22,34 @@ def main():
     # load config
     with open(args.config, 'r') as f:
         parameters = json.load(f)
-    # create experiment 
-    experiment_path = os.path.join(os.getcwd(), 'experiments', args.name)
-    try:
-        os.mkdir(experiment_path)
-    except FileExistsError:
-        # TODO just pass if you don't want this safety prompt
-        if args.name != "test":
-            print(pcolors.RED + '\n"{}" already exists! Continuing will overwrite previous results!'.format(args.name), end='\t')
-            decision = str.lower(input("Continue anyway? [y/N]  "  + pcolors.END)) or "n"
-            print('\n')
-            if decision != 'y':
-                print('Aborting!')
-                return -1
+    # parameters['parsed_args'] = args_dict
+    # with open(os.path.join(experiment_path, 'config.json'), 'w+') as f:
+    #     json.dump(parameters, f, sort_keys=True, indent=0)
     
-    parameters['parsed_args'] = args_dict
-    with open(os.path.join(experiment_path, 'config.json'), 'w+') as f:
-        json.dump(parameters, f, sort_keys=True, indent=0)
+    # volume_dirs = glob.glob(parameters["path_loading"] + "/*")
+    # volume_dirs = (
+    #     r"C:\Users\PhilippsLabLaptop\Desktop\Data_volumeAveraging\recon_rasterVol01_4000x512x511.bin",
+    #     r"C:\Users\PhilippsLabLaptop\Desktop\Data_volumeAveraging\recon_rasterVol02_4000x512x511.bin",
+    #     r"C:\Users\PhilippsLabLaptop\Desktop\Data_volumeAveraging\recon_rasterVol03_4000x512x511.bin" 
+    #     )
 
+    VolReg = vol2d(parameters)
+    v = VolReg._rigid_registration(method="crosscorrelation")
+    VolReg._save_volume_to_disk(v)
+    
+    # # create experiment 
+    # experiment_path = os.path.join(os.getcwd(), 'experiments', args.name)
+    # try:
+    #     os.mkdir(experiment_path)
+    # except FileExistsError:
+    #     # TODO just pass if you don't want this safety prompt
+    #     if args.name != "test":
+    #         print(pcolors.RED + '\n"{}" already exists! Continuing will overwrite previous results!'.format(args.name), end='\t')
+    #         decision = str.lower(input("Continue anyway? [y/N]  "  + pcolors.END)) or "n"
+    #         print('\n')
+    #         if decision != 'y':
+    #             print('Aborting!')
+    #             return -1
     
     print('\nDone with registration.\n')
     
--- src/volumeregistration2dbased.py
+++ src/volumeregistration2dbased.py
@@ -1,173 +1,96 @@
-import cv2
-import scipy
-import os, sys
+import os
+import glob
 import numpy as np
-from PIL import Image
 from tqdm.auto import tqdm 
 from itertools import groupby
 from scipy.ndimage import shift
 import matplotlib.pyplot as plt
+from skimage import registration
+from skimage.registration import phase_cross_correlation
 from image_registration import chi2_shift, cross_correlation_shifts
 
-volume_dirs = (
-    r"C:\Users\PhilippsLabLaptop\Desktop\recon_rasterVol01_4000x512x511.bin",
-    r"C:\Users\PhilippsLabLaptop\Desktop\recon_rasterVol02_4000x512x511.bin",
-    r"C:\Users\PhilippsLabLaptop\Desktop\recon_rasterVol03_4000x512x511.bin"
-    )
 
-def check_size_compatibility(input_list: list) -> bool:
-    """_summary_
+class VolumeRegistration2Dbased():
+    def __init__(self, params_dict) -> None:
+        self.vol_files = glob.glob(params_dict["path_loading"] + "/*")
+        self.path_saving = params_dict['path_saving']
+        self.volume_pre_checks()
+    
+    def all_equal(self, list) -> bool:
+        g = groupby(list)
+        return next(g, True) and not next(g, False)
+    
+    def get_dimensions_from_file_name(self, file: str) -> list:
+        dims = os.path.basename(file).split('_')[-1].split('.bin')[0].split('x')
+        return [int(i) for i in dims]
 
-    Args:
-        input_list (list): _description_
-    Returns:
-        bool: _description_
-    """
-    file_sizes = [os.path.getsize(i) for i in input_list]
-    g = groupby(file_sizes)
-    return next(g, True) and not next(g, False)
+    def volume_pre_checks(self):
+        print("[INFO:] Asserting all file names in list to follow the naming convention \"./../<prefix>_AxBxC.bin\"\nand containing volumes of equal size")
+        file_sizes = [os.path.getsize(i) for i in self.vol_files]
+        if not self.all_equal(file_sizes):
+            raise Exception("File sizes are different!")
+        dims = [self.get_dimensions_from_file_name(i) for i in self.vol_files]
+        if not self.all_equal(dims):
+            raise Exception("Dimensions for files (by file name) are different!")
+        self.dims = dims[0]
+          
+    @staticmethod  
+    def signaltonoise(a, axis=0, ddof=0):
+        a = np.asanyarray(a)
+        m = a.mean(axis)
+        sd = a.std(axis=axis, ddof=ddof)
+        return np.where(sd == 0, 0, m/sd)
 
-
-def get_dimensions_from_file_name(file: str) -> list:
-    """Returns volume dimensions from the file name in which the data is stored
-    Args:
-        file (str): file name from which the string is split
-        asserts the following form for the file path: "../../<prefix>_AxBxC.bin"
-    Returns:
-        list: list with the volume dimensions -> (a,b,c)
-    """
-    dims = os.path.basename(file).split('_')[-1].split('.bin')[0].split('x')
-    return [int(i) for i in dims]
-
-
-def FAST_oriented_BRIEF_2D_registration(img1: np.array, img2: np.array, n_orb_feat: int=250) -> np.array:
-    """TODO: buggy -> produces crazy artifacts
-        img1 is the reference frame onto which the dewarped image is overlayed -> both images are averaged
-    """
-    # assuming img1 and img2 have the same size and color code
-    orb_detector = cv2.ORB_create(n_orb_feat)
-    kp1, d1 = orb_detector.detectAndCompute(img1, None)
-    kp2, d2 = orb_detector.detectAndCompute(img2, None)
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = matcher.match(d1, d2)
-    matches.sort(key = lambda x: x.distance)
-    matches = matches[:int(len(matches)*0.9)]
-    no_of_matches = len(matches)
-    p1 = np.zeros((no_of_matches, 2))
-    p2 = np.zeros((no_of_matches, 2))
-    for i in range(len(matches)):
-        p1[i, :] = kp1[matches[i].queryIdx].pt
-        p2[i, :] = kp2[matches[i].trainIdx].pt
-    homography, _ = cv2.findHomography(p1, p2, cv2.RANSAC)
-    transformed_img = cv2.warpPerspective(img2, homography, (img2.shape[1],img2.shape[0]))
-    # return average of reg imgs and transform
-    return np.asarray(Image.blend(Image.fromarray(img1), Image.fromarray(transformed_img), alpha=0.5)) 
-
-
-def rigid_registration(img1: np.array, img2: np.array, n_orb_feat: int=250) -> np.array:
-    img1 = np.float32(img1)
-    img2 = np.float32(img2)
-    src = cv2.cornerHarris(img1, img1.shape[1]//100, 5, 0.1)
-    dst = cv2.cornerHarris(img2, img2.shape[1]//100, 5, 0.1)
-    #result is dilated for marking the corners, not important
-    src = cv2.dilate(src, None)
-    dst = cv2.dilate(dst, None)
-    # ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
-    # dst = np.uint8(dst)
-    M = cv2.findHomography(src, dst, cv2.RANSAC)
-    # out = cv2.warpAffine(img2, M, img2.shape)
-    # fig, ax = plt.subplots(1,4)
-    # ax[0].imshow(img1)
-    # ax[1].imshow(img2)
-    # ax[2].imshow(dst)
-    # ax[3].imshow(out)
-    # plt.show()
-    return 
-
-
-def signaltonoise(a, axis=0, ddof=0):
-    a = np.asanyarray(a)
-    m = a.mean(axis)
-    sd = a.std(axis=axis, ddof=ddof)
-    return np.where(sd == 0, 0, m/sd)
-
-
-def chi_squared_registration(image: np.array, offset_image: np.array, noise: float=0.1) -> np.ndarray:
-    xoff, yoff, _, _ = chi2_shift(image, offset_image, noise, return_error=True, upsample_factor='auto')
-    return shift(offset_image, shift=(xoff,yoff), mode='constant')
-    fig, ax = plt.subplots(1, 4)
-    ax[0].imshow(image[2500:3500], cmap='gray')
-    ax[0].set_title(f"Input image 1: SNR={np.round(signaltonoise(img1, axis=None), 4)}")
-    ax[1].imshow(offset_image[2500:3500], cmap='gray')
-    ax[1].set_title(f"Input image 1: SNR={np.round(signaltonoise(offset_image, axis=None),4)}")
-    ax[2].imshow(corrected_image[2500:3500], cmap='gray') 
-    ax[2].set_title(f"Chi Squared corrected image\n(offset_image registered to img 1): SNR={np.round(signaltonoise(corrected_image, axis=None), 4)}")
-    ax[3].imshow(((image+corrected_image)/2)[2500:3500], cmap='gray')
-    ax[3].set_title(f"Average of Img1 and the\ncorrected image: SNR={np.round(signaltonoise((image+corrected_image)/2, axis=None), 4)}")
-    plt.show()
-    return
-
-
-def cross_correlation_registration(image: np.array, offset_image: np.array) -> np.ndarray:
-    xoff, yoff = cross_correlation_shifts(image, offset_image)
-    corrected_image = shift(offset_image, shift=(xoff,yoff), mode='constant')    
-    # return corrected_image
-    fig, ax = plt.subplots(1, 4)
-    ax[0].imshow(image[2500:3500], cmap='gray')
-    ax[0].set_title(f"Input image 1: SNR={np.round(signaltonoise(image, axis=None), 4)}")
-    ax[1].imshow(offset_image[2500:3500], cmap='gray')
-    ax[1].set_title(f"Input image 1: SNR={np.round(signaltonoise(offset_image, axis=None),4)}")
-    ax[2].imshow(corrected_image[2500:3500], cmap='gray') 
-    ax[2].set_title(f"Chi Squared corrected image\n(img2 registered to img 1): SNR={np.round(signaltonoise(corrected_image, axis=None), 4)}")
-    ax[3].imshow(((image+corrected_image)/2)[2500:3500], cmap='gray')
-    ax[3].set_title(f"Average of Img1 and the\ncorrected image: SNR={np.round(signaltonoise((image+corrected_image)/2, axis=None), 4)}")
-    plt.show()
-    return
-
-def _registration()
-
-def register_n_vols_slice_wise(input_list: list, method: str='noreg', axis=-1) -> np.array:
-    if not check_size_compatibility(input_list):
-        return # implement error with explanation of what went wrong
-    dims = get_dimensions_from_file_name(input_list[0]) # rethink reshaping
-    buffer_size_pxls = dims[0] * dims[1]
-    out_vol = np.zeros((tuple(dims)), dtype=np.uint8)
-    tmp_buffer_stack = np.zeros((dims[0], dims[1], len(input_list)), dtype=np.uint8)
-    out_buffer_stack = np.zeros((dims[0], dims[1], len(input_list)))
-    for i in tqdm(range(dims[-1])):
-        for j, _ in enumerate(input_list):
-            with open(input_list[j]) as c_file: # fill current buffer cluster (B-scans that we want to register)
-                tmp_buffer_stack[...,j] = np.fromfile(c_file, 
-                                                dtype=np.uint8,
-                                                count=buffer_size_pxls,
-                                                offset=i * buffer_size_pxls).reshape((dims[1], dims[0])).swapaxes(0,1)    
-        out_buffer_stack[...,0] = tmp_buffer_stack[...,0]
-        if method=='noreg': # average volume WITHOUT ANY REGISTRATION
-            out_vol[:,:,i] = np.mean(tmp_buffer_stack, axis=-1)  
-        elif method=='fastbrief': # none-rigid registration -> currently produces crazy artifacts --> TODO: debug
-            for k in range(len(input_list)-1): # iterate through all n volumes/current frame in volume and register all against 1st/reference frame
-                out_buffer_stack[:,:,k] = FAST_oriented_BRIEF_2D_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1]) # perform registration
-            out_vol[:,:,i] = np.mean(out_buffer_stack, axis=-1)
-        # elif method=='rigid':
-        #     rigid_registration(reg_buffer_stack[:,:,0], reg_buffer_stack[:,:,1])
-        elif method=='chisquared':
-            for k in range(len(input_list)-1): # iterate through all n volumes/current frame in volume and register all against 1st/reference frame
-                out_buffer_stack[:,:,k] = chi_squared_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1]) # perform registration
-            out_vol[:,:,i] = np.mean(out_buffer_stack, axis=-1)
-        elif method=='crosscorrelation':
-            for k in range(len(input_list)-1): # iterate through all n volumes/current frame in volume and register all against 1st/reference frame
-                out_buffer_stack[:,:,k] = chi_squared_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1]) # perform registration
-            out_vol[:,:,i] = np.mean(out_buffer_stack, axis=-1)
+    @staticmethod
+    def chi_squared_registration(image: np.array, offset_image: np.array, noise: float=0.1) -> np.ndarray:
+        xoff, yoff, _, _ = chi2_shift(image, offset_image, noise, return_error=True, upsample_factor='auto')
+        return shift(offset_image, shift=(xoff,yoff), mode='constant')
+     
+    @staticmethod   
+    def cross_correlation_registration(image: np.array, offset_image: np.array) -> np.ndarray:
+        xoff, yoff = cross_correlation_shifts(image, offset_image)
+        return shift(offset_image, shift=(xoff,yoff), mode='constant')    
+    
+    @staticmethod
+    def optical_flow_registration(image: np.array, offset_image: np.array) -> np.array:
+        flow = registration.optical_flow_tvl1(image, offset_image)
+        flow_x, flow_y = flow[1, :, :], flow[0, :, :]
+        xoff, yoff = np.mean(flow_x), np.mean(flow_y)
+        return shift(offset_image, shift=(xoff,yoff), mode='constant')
         
-    return out_vol    
-
-
-def save_averaged_volume(vol: np.array, path: str):
-    assert os.path.isfile(path)
-    np.swapaxes(vol, 0, -1).astype(np.uint8).tofile(path)
-    return
-
-
-vol = register_n_vols_slice_wise(volume_dirs, 'crosscorrelation')
-save_averaged_volume(vol, r"C:\Users\PhilippsLabLaptop\Desktop\crosscorr_vol_bidirectional.bin")
-# np.swapaxes(v3, 0, -1).astype(np.uint8).tofile()
+    @staticmethod
+    def translation_registration(image: np.array, offset_image: np.array) -> np.array:
+        shifted, _, _ = phase_cross_correlation(image, offset_image)
+        xoff, yoff = -shifted[1], -shifted[0]
+        return shift(offset_image, shift=(xoff,yoff), mode='constant')
+    
+    def _rigid_registration(self, method: str="chisquared") -> np.array:
+        buffer_size_pxls = self.dims[0] * self.dims[1]
+        out_vol = np.zeros((tuple(self.dims)), dtype=np.uint8)
+        tmp_buffer_stack = np.zeros((self.dims[0], self.dims[1], len(self.vol_files)), dtype=np.uint8)
+        out_buffer_stack = np.zeros((self.dims[0], self.dims[1], len(self.vol_files)))
+        for i in tqdm(range(self.dims[-1])):
+            for j, _ in enumerate(self.vol_files):
+                with open(self.vol_files[j]) as c_file: # fill current buffer cluster (B-scans that we want to register)
+                    tmp_buffer_stack[...,j] = np.fromfile(c_file, dtype=np.uint8, count=buffer_size_pxls, 
+                                                          offset = i * buffer_size_pxls).reshape((self.dims[1], self.dims[0])).swapaxes(0,1)    
+            out_buffer_stack[...,0] = tmp_buffer_stack[...,0]
+            for k in range(len(self.vol_files)-1): # iterate through all n volumes/current frame in volume and register all against 1st/reference frame
+                if method=='noreg': # average volume WITHOUT ANY REGISTRATION
+                    out_vol[:,:,i] = np.mean(tmp_buffer_stack, axis=-1)
+                elif method=='chisquared':
+                    out_buffer_stack[:,:,k] = VolumeRegistration2Dbased.chi_squared_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1])
+                elif method=='crosscorrelation':
+                    out_buffer_stack[:,:,k] = VolumeRegistration2Dbased.cross_correlation_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1]) 
+                elif method=='opticalflow':
+                    out_buffer_stack[:,:,k] = VolumeRegistration2Dbased.optical_flow_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1]) 
+                elif method=='registrationtranslation':
+                    out_buffer_stack[:,:,k] = VolumeRegistration2Dbased.translation_registration(tmp_buffer_stack[:,:,0], tmp_buffer_stack[:,:,k+1])
+            out_vol[:,:,i] = np.mean(out_buffer_stack, axis=-1)
+        print(f"\nSNR of raw volume={VolumeRegistration2Dbased.signaltonoise(out_buffer_stack)}\nand SNR of registered volume={VolumeRegistration2Dbased.signaltonoise(out_vol)}")
+        return out_vol
+            
+    def _save_volume_to_disk(self, out_vol):
+        print("[INFO:] Saving to disk...")
+        np.swapaxes(out_vol, 0, -1).astype(np.uint8).tofile(self.path_saving) #TODO: grab that from config
+        print("[INFO:] Done!")
